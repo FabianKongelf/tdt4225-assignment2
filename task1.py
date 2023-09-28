a48b5e4f568a9d5c0c41f335f437dbe60708e48d
Fabian Kongelf <fkongelf@gmail.com>
2023-09-28 13:41:30 +0200

    f

--- task1.py
+++ task1.py
@@ -11,14 +11,14 @@ class Task1():
         self.cursor.execute(query)
         self.db_connection.commit()
 
-    def insert_data(self, table_name):
-        names = ['Bobby', 'Mc', 'McSmack', 'Board']
-        for name in names:
-            # Take note that the name is wrapped in '' --> '%s' because it is a string,
-            # while an int would be %s etc
-            query = "INSERT INTO %s (name) VALUES ('%s')"
-            self.cursor.execute(query % (table_name, name))
-        self.db_connection.commit()
+    # def insert_data(self, table_name):
+    #     names = ['Bobby', 'Mc', 'McSmack', 'Board']
+    #     for name in names:
+    #         # Take note that the name is wrapped in '' --> '%s' because it is a string,
+    #         # while an int would be %s etc
+    #         query = "INSERT INTO %s (name) VALUES ('%s')"
+    #         self.cursor.execute(query % (table_name, name))
+    #     self.db_connection.commit()
 
 def main():
     program = None
@@ -55,13 +55,13 @@ def main():
         except Exception as e:
             print("ERROR: Failed to create Table: ", e)
         
-        finally:
-            # insert data
-            try:
-                program.insert_data()
+        # finally:
+        #     # insert data
+        #     try:
+        #         program.insert_data()
 
-            except Exception as e:
-                print("ERROR: Failed inserting data: ", e)
+        #     except Exception as e:
+        #         print("ERROR: Failed inserting data: ", e)
 
 
         
