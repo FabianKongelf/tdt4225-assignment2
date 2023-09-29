0b5f01b8ba93219bd195466487f65422ffd32514
Fabian Kongelf <fkongelf@gmail.com>
2023-09-29 12:06:40 +0200

    f

--- task1.py
+++ task1.py
@@ -14,7 +14,9 @@ class Task1():
 
     def insert_data(self, query, data):
         self.cursor.executemany(query, data)
+        last_insert_id = self.cursor.lastrowid
         self.db_connection.commit()
+        return last_insert_id
 
 def main():
     program = None
@@ -78,7 +80,7 @@ def main():
                     has_labels = 1 if user_id in labels else 0
                     user_list_converted.append((user_id, has_labels))
 
-                # program.insert_data("""INSERT INTO user (id, has_label) VALUES (%s, %s)""", user_list_converted)
+                # _ = program.insert_data("""INSERT INTO user (id, has_label) VALUES (%s, %s)""", user_list_converted)
 
             except Exception as e:
                 print("ERROR: Failed inserting users: ", e)
@@ -88,41 +90,74 @@ def main():
             # insert activitys
             try:
                 for user in user_list_converted:
-                    id = user[0]
-                    user_data_path = os.path.join(user_data, id)
+                    user_id = user[0]
+                    user_data_path = os.path.join(user_data, user_id)
                     # print(user)
                     
+                    # if user has labels read them
+                    lables = []
                     if user[1] == 1:
-                        # inseter labels
-                        activities = []
+                        # inseter labels    
                         labels_path = os.path.join(user_data_path, "labels.txt")
                         with open(labels_path, "r") as file:
                             lines = file.readlines()[1:]
                             for line in lines:
                                 array_line = line.strip().split("\t")
                                 array_line.append(user[0])
-                                activities.append(array_line)
+                                lables.append(array_line)
 
-                        program.insert_data("""INSERT INTO activity (start_date_time, end_date_time, transportation_mode, user_id) VALUES (%s, %s, %s, %s)""", activities)
-
+                    # find all files assosiaded with user
                     files_path = os.path.join(user_data_path, "Trajectory")
                     files = os.listdir(files_path)
-                
+
+                    # loop on all files for user
                     for file_name in files:
                         file_path = os.path.join(files_path, file_name)
 
+                        # read file data
                         file_data = []
                         with open(file_path, "r") as file:
                             lines = file.readlines()[6:]
                             for line in lines:
                                 converted_line = line.strip().split(",")
+
+                                # correct datatime 
                                 datetime = converted_line[-2] + " " + converted_line[-1]
                                 converted_line[-2:] = [datetime]
+
+                                # ajust altitute
+                                if converted_line[3] == -777:
+                                    if len(file_data) > 0:
+                                        converted_line[3] = file_data[-1][2]
+                                    else:
+                                        converted_line[3] = 0
+
+                                # remove field 3
+                                converted_line.pop(2)
+
                                 file_data.append(converted_line)
                         
-                        if user[1] == 0:
-                            user_activity = [id, file_data[0][5], file_data[-1][5]]
-
+                        # ignorre data if longer then 2500
+                        if len(file_data) > 2500:
+                            continue
+                
+                        # create user activity
+                        user_activity = [user_id, file_data[0][4], file_data[-1][4]]
+                        query = """INSERT INTO activity (user_id, start_date_time, end_date_time) VALUES (%s, %s, %s)"""
+              
+                        # if user has labels
+                        if user[1] == 1:
+                            for row in lables:
+                                if row[0] == file_data[0][4] and row[1] == file_data[-1][4]:
+                                    user_activity = [user_id, row[2], file_data[0][4], file_data[-1][4]]
+                                    query = """INSERT INTO activity (user_id, transportation_mode, start_date_time, end_date_time) VALUES (%s, %s, %s, %s)"""
+                        
+                        # insert activity
+                        activity_id = program.insert_data(query, [user_activity])
+                        
+                        # insert trackpoint
+                        new_file_data = [inner_array + [activity_id] for inner_array in file_data]
+                        _ = program.insert_data("""INSERT INTO trackpoint (lat, lon, altitude, date_days, date_time, activity_id) VALUES (%s, %s, %s, %s, %s, %s)""", new_file_data)
 
                         # print(content)
                     print("-- inserted data for: " + user[0])
@@ -130,7 +165,7 @@ def main():
             except Exception as e:
                 print("ERROR: Failed inserting activity: ", e)
             finally:
-                print("-- inserted activity")
+                print("-- insertes done")
     
     # failed init program
     except Exception as e:
--- test.py
+++ test.py
@@ -1,11 +1,25 @@
-# Input string
-input_string = "2008/08/20 12:09:17 2008/08/20 12:45:05 walk"
+array = [
+    [0, 1, 2, 3],
+    [0, 1, 2, 3],
+    [0, 1, 2, 3],
+    [0, 1, 2, 3],
+    [0, 1, 2, 3],
+    [0, 1, 2, 3],
+    [0, 1, 2, 3],
+    [0, 1, 2, 3],
+    [0, 1, 2, 3],
+    [0, 1, 2, 3]
+]
 
-# Split the string on spaces
-split_parts = input_string.split()
+column_index = 3  # Index of the column you want to extract
 
-# Combine date and time parts and append "walk"
-result_list = [split_parts[0] + " " + split_parts[1], split_parts[2] + " " + split_parts[3], split_parts[4]]
+# Initialize an empty 1D array
+result = []
 
-# Print the resulting list
-print(result_list)
+# Iterate through the rows and append values from the specified column
+for row in array:
+    if len(row) > column_index:
+        value = row[column_index]
+        result.append(value)
+
+print(result)
